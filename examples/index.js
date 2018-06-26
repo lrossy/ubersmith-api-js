18ff992e448c0544111a382694e6315c1230ca68
Luke <lrossy@gmail.com>
2018-06-26 14:01:51 -0400

    * fixed api return

--- examples/index.js
+++ examples/index.js
@@ -2,15 +2,9 @@
     let Ubersmith = require('../lib');
     let client = new Ubersmith('https://192.168.2.182', 'admin', 'tZhFqYqQwy4Xqkzh3V2yLfKpKh766T34');
 
-    let request = client.call();
-
-
-    console.log('here')
-    // client.request.send(request,function(err, result){
-    //     console.log(result);
-    //     console.log('Error: '+ err);
-    //     console.log('Errors: ');
-    //     console.log(result.errors)
-    // });
+    let request = client.call()
+        .then(r =>{
+            console.log('here', r)
+        });
 
 }).call(this);
